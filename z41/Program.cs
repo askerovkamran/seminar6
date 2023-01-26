caea373851cf1a7f0ba80a54a1eea1bcddff3ebf
kamran <askerari@mail.ru>
2023-01-26 18:20:14 +0300

    Решил задачу

---
+++ z41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
+
+
+
+Console.WriteLine("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
